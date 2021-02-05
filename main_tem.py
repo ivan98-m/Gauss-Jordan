4abf438cf9b088430e39ad122af4e02380458d30
Ivan Morales <ivan_98m@outlook.com>
2021-02-05 18:10:03 -0500

    validaciones 3x3, gradicar 3d

--- main_tem.py
+++ main_tem.py
@@ -6,6 +6,7 @@ from PyQt5.QtGui import QCloseEvent
 from interfaz.Principal11_ui import *
 from pythonsrc.validar_campos import Validar
 from pythonsrc.graficar_2D import graficar_2d
+from pythonsrc.graficar_3D import graficar_3d
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -24,27 +25,26 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def validar_dimension_matriz(self, text):
         if (text == "2x2"):
-            #self.validar.lista2
+            self.limpiar()
             self.abrir_2x2()
         elif (text == "3x3"):
+            self.limpiar()
             self.abrir_3x3()
         elif (text == "4x4"):
-            print("4x4")
+            self.abrir_4x4()
         elif (text == "5x5"):
             print("5x5")
 
     def abrir_2x2(self):
-        print(self.validar.prueba)
         print("2x2")
         self.validar.abrir_interfaz_2x2()
         self.textsolucion.setText("DOBLE CLICKkkk")
         #self.textecuaciones.setText(str(matriz))
         #self.imprimir_ecuaciones()
         #self.validar.dialogo.cl
-
-        btn=self.metodo
         self.graficar.setEnabled(True)
         self.graficar.clicked.connect(self.graficar_ecuaciones_2d)
+        btn=self.metodo
         #self.textecuaciones.setEnabled(True)
         #ecuaciones="2 + 4 = 8 \n5 - 6 = 9"
         #self.textecuaciones.setPlainText(ecuaciones)
@@ -52,7 +52,18 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             print(btn+" seleccionado")
         elif(btn == "Otro metodo"):
             print(btn+" seleccionado")
-    
+
+    def abrir_3x3(self):
+        print("#Dimencion de matriz 3x3")
+        self.validar.abrir_interfaz_3x3()
+        self.textsolucion.setText("SOLUCION 3 VAR 3 INCOG")
+        self.graficar.setEnabled(True)
+        self.graficar.clicked.connect(self.graficar_ecuaciones_3d)
+
+    def abrir_4x4(self):
+        self.graficar.setEnabled(False)
+        print("#Dimencion de matriz 4x4")
+
     def imprimir_ecuaciones(self):
         cad=""
         for i in self.validar.lista2:
@@ -69,8 +80,15 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             print(i)
         graficar_2d(self.validar.lista2)
 
-    def abrir_3x3(self):
-        print("#Dimencion de matriz 3x3")
+    def graficar_ecuaciones_3d(self):
+        print("valores mandados para graficar")
+        for i in self.validar.lista2:
+            print(i)
+        graficar_3d(self.validar.lista2)
+
+    def limpiar(self):
+        self.textecuaciones.clear()
+        self.textsolucion.clear()
     
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
