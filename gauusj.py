99820c72d3445170e1f0f1fb80da98c61e9daa0c
Juaneco-1 <junnior25a@hotmail.com>
2021-02-19 02:03:12 -0500

    Gauus-Jordan funcionando y añadido el proceso de desarrollo

--- gauusj.py
+++ gauusj.py
@@ -19,13 +19,65 @@ def lista_inicial(lista):
         
         if (np.linalg.det(a)==0):
             print ("No hay solucion el sistema es inconsitente")
+            return 0,0,"No hay solucion el sistema es inconsitente"
         else:
+            #print(np.linalg.det(a))
             print ("Aqui va la sol")
             X,A,sol=gaussJordan2(a,b)
             """ print(X)
             print(A) """
+            return X,A,sol
             #print(gaussJordan(a,b))
-    return X,A,sol
+    elif(longitud==12):
+        matriz=np.array(lista, dtype='float').reshape(3,4)
+        #matriz=np.reshape(lista,(2,3))
+        #a=np.reshape(matriz,(2,2))
+        b=matriz[:,3] #Igual de la ecuacion
+        a=np.delete(matriz,3,axis=1)# Matriz de coeficientes
+        #print(b)
+        #print(a)
+        
+        if (np.linalg.det(a)==0):
+            print ("No hay solucion el sistema es inconsitente")
+            return 0,0,"No hay solucion el sistema es inconsitente"
+        else:
+            print ("Aqui va la sol")
+            X,A,sol=gaussJordan2(a,b)
+            return X,A,sol
+    elif (longitud==20):
+        matriz=np.array(lista, dtype='float').reshape(4,5)
+        #matriz=np.reshape(lista,(2,3))
+        #a=np.reshape(matriz,(2,2))
+        b=matriz[:,4] #Igual de la ecuacion
+        a=np.delete(matriz,4,axis=1)# Matriz de coeficientes
+        #print(b)
+        #print(a)
+        
+        if (np.linalg.det(a)==0):
+            print ("No hay solucion el sistema es inconsitente")
+            return 0,0,"No hay solucion el sistema es inconsitente"
+        else:
+            print ("Aqui va la sol")
+            X,A,sol=gaussJordan2(a,b)
+            return X,A,sol
+    elif (longitud==30):
+        matriz=np.array(lista, dtype='float').reshape(5,6)
+        #matriz=np.reshape(lista,(2,3))
+        #a=np.reshape(matriz,(2,2))
+        b=matriz[:,5] #Igual de la ecuacion
+        a=np.delete(matriz,5,axis=1)# Matriz de coeficientes
+        #print(b)
+        #print(a)
+        
+        if (np.linalg.det(a)==0):
+            print ("No hay solucion el sistema es inconsitente")
+            return 0,0,"No hay solucion el sistema es inconsitente"
+        else:
+            
+            print ("Aqui va la sol")
+            X,A,sol=gaussJordan2(a,b)
+            return X,A,sol
+        
 def gaussJordan2(matriz, vector):
     solucion=""
     print (matriz)
@@ -42,30 +94,60 @@ def gaussJordan2(matriz, vector):
     
     n=len(b)
     print("NUMERO DE INCOGNITAS: "+str(n))
-    
+    solucion+="\n NUMERO DE INCOGNITAS: "+str(n)
     
     for k in range (n):
         
         if(np.fabs(a[k,k]) < 1.0e-12):
+            solucion+="\nEl pivote de la primera fila es 0 por lo tanto se hace el cambio con una fila >0"
+            print("El pivote de la primera fila es 0 por lo tanto se hace el cambio con una fila >0")
             for i in range(k+1,n):
                 if (np.fabs(a[i,k]) > np.fabs(a[k,k])):
+                    print("Fila Elejida :"+str(i+1))
+                    solucion+="\nFila Elejida :"+str(i+1)
                     for j in range(k,n):
+                        print("Con valores :"+str(a[i,j]))
+                        solucion+="\nCon valores :"+str(a[i,j])
                         a[k,j],a[i,j]=a[i,j],a[k,j]
                     b[k],b[i]=b[i],b[k]
+                    solucion+="\nNueva Matriz de coeficientes : \n"+str(a)
+                    solucion+="\nNueva vector solución : \n"+str(b)
+                    print(a)
+                    print (b)
                     break
         
         #Division  de la fila pivote
+        solucion+="\n\nEn la iteracion: "+str(k)
+        print("\n En la iteracion: "+str(k))
         pivote=a[k,k]
+        
         print("EL PIVOTE ES: "+str(pivote))
+        solucion+="\n EL PIVOTE ES: "+str(pivote)
+        
         for j in range(k,n):
+            
+            solucion+="\n\nDividimos los elementos de la fila: "+str(k)+" "+"sobre el pivote "+str(pivote)    
             a[k,j] /=pivote
+            solucion+="\n\nLuego de la operacion: "+str(a[k,j])
+        solucion+="\n Nueva matriz de coeficientes: \n"+str(a)
         b[k] /=pivote
+        solucion+="\n Vector solucion luego de la op : \n"+str(b[k])
+        solucion+="\nNuevo vector solucion: \n"+str(b)
+        """ for i in range(k):
+            print("entro")
+            solucion+="\nLuego de la operacion: "+str(a[k,i]) """
         #Ciclo de elimacion
+        solucion+="\n\nELIMINACION DE LAS FILAS CON VALOR DISTINTO A 0 RESPECTO AL ELEMENTO PIVOTE"
         for i in range(n):
             if(i==k or a[i,k] == 0): continue #verifico que el elemento no sea de la diagonal o sea 0
+            
             factor=a[i,k]
+            solucion+="\nFactor para la eliminación: "+str(factor)
             for j in range(k,n):
+                solucion+="\nNuevo Elemento"+"="+str(a[i,j])+"-"+str(factor)+"*"+str(a[k,j])
                 a[i,j] -=factor*a[k,j]
+                
+                solucion+="\nLuego de la operación: "+str(a[i,j])
             b[i]-=factor*b[k]
     return b,a,solucion
 def gaussJordan(matriz, vector):
@@ -100,7 +182,7 @@ def gaussJordan(matriz, vector):
 
     return x
 
-
+lista_inicial([0,1,3,4,5,6])
 """ orden=input("Orden del sistema :")
 lista=[]
 if(orden == "2"):
